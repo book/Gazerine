da87c876032419a95abc1188493d8f682c362f23
Philippe Bruhat (BooK) <book@cpan.org>
2017-11-14 23:39:58 +0100

    Create a first, basic datastore (memory) for testing

---
+++ lib/Gazerine/DataStore/Memory.pm
@@ -0,0 +1,34 @@
+package Gazerine::DataStore::Memory;
+
+use List::Util qw< max >;
+
+use Moo;
+use namespace::clean;
+
+with 'Gazerine::Role::DataStore';
+
+my %storage;
+my $name = 'memory1';
+
+has 'name' => (
+    is       => 'ro',
+    default  => sub { $name++ },
+    required => 1,
+);
+
+sub _next_id_ {
+    return 1 + ( max( keys %{ $storage{ $_[0]->name }{ $_[1] } } ) // 0 );
+}
+
+sub create_entity {
+    my ( $self, $class, $data ) = @_;
+
+    # compute the new id and store the data
+    my $_id_ = $self->_next_id_($class);
+    $storage{ $self->name }{$class}{$_id_} = { %$data, _id_ => $_id_ };
+
+    # return a new object
+    return $class->new( %$data, _id_ => $_id_ );
+}
+
+1;
