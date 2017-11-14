2c9c526f0d50905be0631185e9a27c92b9508a07
Philippe Bruhat (BooK) <book@cpan.org>
2017-11-14 23:37:32 +0100

    Create the Person, the most important entity in the system

---
+++ lib/Gazerine/Person.pm
@@ -0,0 +1,17 @@
+package Gazerine::Person;
+
+use Moo;
+use namespace::clean;
+
+with 'Gazerine::Role::Entity';
+
+has name => (
+    is       => 'ro',
+    required => 1,
+);
+
+sub as_hashref {
+    return { name => $_[0]->name };
+}
+
+1;
